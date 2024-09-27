2758fe61b66e8df2e68d589cfd9f87e530f81bb1
Sanraku95 <demetriusgit@gmail.com>
2024-09-27 19:56:47 +0400

    feat: add navbar

---
+++ asvla/src/components/forge/MainComponent.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./MainComponent.scss";
+import PrimarySearchAppBar from "./nav/PrimarySearchAppBar";
+
+const MainComponent: React.FC = () => {
+  return (
+    <div>
+      <PrimarySearchAppBar />
+    </div>
+  );
+};
+
+export default MainComponent;
