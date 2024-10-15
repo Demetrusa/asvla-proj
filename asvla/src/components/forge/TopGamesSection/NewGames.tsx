8b165ea8c1ee11938b24768c825b738330b2d710
Sanraku95 <demetriusgit@gmail.com>
2024-10-15 15:48:01 +0400

    Feat: add new section

--- asvla/src/components/forge/TopGamesSection/NewGames.tsx
+++ asvla/src/components/forge/TopGamesSection/NewGames.tsx
@@ -2,18 +2,24 @@ import React from "react";
 import "./TopGames.scss";
 import { Link } from "react-router-dom";
 
-const NewGames: React.FC = () => {
+interface NewGamesCardProps {
+  imageSrc: string;
+  link: string;
+}
+
+const NewGames: React.FC<NewGamesCardProps> = ({ imageSrc, link }) => {
   return (
-    <div className="newGamesSection">
-      <div className="newGamesSection__inside">
-        <img src="" alt="" />
-        <div className="newGames__title">
-          <span>ახალი</span>
-          <span>ორმაგი ნაბიჯი</span>
+    <Link to={link} className="newGamesLink">
+      <div className="newGamesSection">
+        <div className="newGamesSection__inside">
+          <img src={imageSrc} alt="" />
+          <div className="newGames__title">
+            <span>ახალი</span>
+          </div>
+          <div className="playButton"></div>
         </div>
-        <div className="playButton"></div>
       </div>
-    </div>
+    </Link>
   );
 };
 
--- asvla/src/components/forge/TopGamesSection/TopGames.tsx
+++ asvla/src/components/forge/TopGamesSection/TopGames.tsx
@@ -17,6 +17,12 @@ import slotimg5 from "./TopGamesIMg/kingdom-of-atlantis-jackpot-play-v.webp";
 import slotimg6 from "./TopGamesIMg/regal-spins-10-vertt.webp";
 import slotimg7 from "./TopGamesIMg/amazon's-battlevert.webp";
 import slotimg8 from "./TopGamesIMg/40-super-hot-vertic.webp";
+import slogImg9 from "./TopGamesIMg/monkey_kingdom-horiz_.webp";
+import slogImg10 from "./TopGamesIMg/big-chill.webp";
+import slogImg11 from "./TopGamesIMg/the-wild-rhino-horiz_.webp";
+import slogImg12 from "./TopGamesIMg/wizard_blizzard_x5horiz_.webp";
+import slogImg13 from "./TopGamesIMg/queen_of_flames-horiz.webp";
+import slogImg14 from "./TopGamesIMg/fruits_sweets-horiz.webp";
 import NewGames from "./NewGames";
 
 const topGamesData = [
@@ -30,6 +36,14 @@ const topGamesData = [
   { imageSrc: slotimg8, link: "https://example.com/play-game" },
 ];
 
+const NewGamesData = [
+  { imageSrc: slogImg9, link: "https://example.com/play-game" },
+  { imageSrc: slogImg10, link: "https://example.com/play-game" },
+  { imageSrc: slogImg11, link: "https://example.com/play-game" },
+  { imageSrc: slogImg12, link: "https://example.com/play-game" },
+  { imageSrc: slogImg13, link: "https://example.com/play-game" },
+  { imageSrc: slogImg14, link: "https://example.com/play-game" },
+];
 const TopGames: React.FC = () => {
   return (
     <div className="topGamesSection">
@@ -70,7 +84,32 @@ const TopGames: React.FC = () => {
           <span>ახალი თამაშები</span>
           <a href="#">მეტის ნახვა &gt;</a>
         </div>
-        <NewGames />
+        <div className="NewGamesSwiper">
+          <Swiper
+            modules={[Navigation, Pagination, Scrollbar, A11y]}
+            spaceBetween={5}
+            slidesPerView={5}
+            loop={true}
+            navigation={{
+              nextEl: ".NewGames-swiper-button-next",
+              prevEl: ".NewGames-swiper-button-prev",
+            }}
+            onSlideChange={() => console.log("slide change")}
+            onSwiper={(swiper) => console.log(swiper)}
+          >
+            {NewGamesData.map((game, index) => (
+              <SwiperSlide key={index}>
+                <NewGames imageSrc={game.imageSrc} link={game.link} />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+          <div className="NewGames-swiper-button-next">
+            <FontAwesomeIcon icon={faAngleLeft} />
+          </div>
+          <div className="NewGames-swiper-button-prev">
+            <FontAwesomeIcon icon={faAngleLeft} />
+          </div>
+        </div>
       </div>
     </div>
   );
