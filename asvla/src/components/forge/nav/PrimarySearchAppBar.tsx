19eb99ff5f27044b5605cecca2b890705d517024
Sanraku95 <demetriusgit@gmail.com>
2024-10-23 23:30:41 +0400

    feat: add new navigation styles

--- asvla/src/components/forge/nav/PrimarySearchAppBar.tsx
+++ asvla/src/components/forge/nav/PrimarySearchAppBar.tsx
@@ -1,29 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faAddressCard } from "@fortawesome/free-solid-svg-icons";
+import "./PrimarySearchAppBar.scss";
 
 const PrimarySearchAppBar: React.FC = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/promo">Promo</Link>
-        </li>
-        <li>
-          <Link to="/top-games">Top Games</Link>
-        </li>
-        <li>
-          <Link to="/week-games">Week Games</Link>
-        </li>
-        <li>
-          <Link to="/register">Register</Link>
-        </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-      </ul>
+    <nav className="navigationHeader">
+      <div>
+        <Link to="/">Home</Link>
+      </div>
+      <div>
+        <Link to="/promo">Promo</Link>
+      </div>
+      <div>
+        <Link to="/top-games">Top Game</Link>
+      </div>
+      <div>
+        <Link to="/week-games">Week Games</Link>
+      </div>
+      <div className="sighIn">
+        <div className="RegissTerLogo">
+          <Link to="/register">
+            <span className="desktopLoginText">Register</span>
+            <FontAwesomeIcon icon={faAddressCard} className="mobileLogin" />
+          </Link>
+        </div>
+        <div className="LoginLogo">
+          <Link to="/login">
+            <span className="desktopLoginText">Login</span>
+            <FontAwesomeIcon icon={faUser} className="mobileLogin" />
+          </Link>
+        </div>
+      </div>
     </nav>
   );
 };
